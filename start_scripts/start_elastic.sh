591f7c5c1f59c035aca09cc96a8f2cc3e1c71c08
Craig Rowland <38228146+sandflysecurity@users.noreply.github.com>
2020-03-26 09:21:44 +1300

    Version 2.5.2

--- start_scripts/start_elastic.sh
+++ start_scripts/start_elastic.sh
@@ -30,4 +30,4 @@ docker run --mount source=sandfly-elastic-db-vol,target=/usr/share/elasticsearch
 --security-opt="no-new-privileges:true" \
 --network sandfly-net \
 --name elasticsearch \
--t docker.elastic.co/elasticsearch/elasticsearch:7.6.0
+-t docker.elastic.co/elasticsearch/elasticsearch:7.6.1
