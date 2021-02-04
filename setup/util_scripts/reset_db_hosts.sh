7cc1f4f441b178ba9105b3c05718c2439f0daf48
Sandfly Security <38228146+sandflysecurity@users.noreply.github.com>
2021-02-04 21:35:03 +1300

    Hosts purge utility script added.

---
+++ setup/util_scripts/reset_db_hosts.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# Sandfly Security LTD www.sandflysecurity.com
+# Copyright (c) 2016-2021 Sandfly Security LTD, All Rights Reserved.
+
+# This will delete all hosts from the Sandfly DB. This can be used to purge indices if some kind of corruption
+# of the host data has occurred.
+
+SETUP_DATA=../setup_data
+
+# Use standard docker image unless overriden.
+if [[ -z "${SANDFLY_MGMT_DOCKER_IMAGE}" ]]; then
+  VERSION=$(cat ../../VERSION)
+  SANDFLY_MGMT_DOCKER_IMAGE="quay.io/sandfly/sandfly-server-mgmt:$VERSION"
+fi
+
+# Setup elasticsearch server name to custom here if needed.
+ELASTIC_SERVER_URL=$(cat $SETUP_DATA/elastic.server.url.txt)
+# Uncomment and change this if you wish to override what elastic DB for Sandfly so to use. The default is to use
+# sandfly container version, but you can use your own cluster if you wish.
+#export ELASTIC_SERVER_URL="http://elasticsearch.example.com:9200"
+export ELASTIC_SERVER_URL
+
+docker network create sandfly-net 2>/dev/null
+docker rm sandfly-server-mgmt
+
+docker run --name sandfly-server-mgmt \
+--network sandfly-net \
+-e ELASTIC_SERVER_URL \
+-it $SANDFLY_MGMT_DOCKER_IMAGE /usr/local/sandfly/utils/init_hosts_db.sh
+
+
+
